5c77e6ad710139256521670b22f03c9d300f6ee8
Emmzytech <emmzytech@gmail.com>
2021-04-11 08:48:23 +0100

    My Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,72 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
+import "../styles/global.css"
+import Learn from "../images/learn.svg"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Credit from "../images/credit.svg"
+import Map from "../images/map.svg"
+import Ballon from "../images/ballon.svg"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+export default function Home() {
+    document.title = "CapacityBay | Welcome"
+  return <section className="future">
+      <div className="container">
+          <div className="row">
+              <div className="col-sm-12 text-white">
+              <img className="balloon-icon d-none d-lg-block d-xl-block" 
+              src={Ballon} alt="air balloon icon" />
+              <h2>Plan Your Future</h2>
+              <div className="future-texts">
+                        <p>Get connected with everything you need to apply to college, research financial aid and
+                            scholarships, and get advice from counselors, advisors and mentors.</p>
+                    </div>
+              </div>
+          </div>
+          <div className="row">
+              <div className="col-sm-4">
+                  <div className="col">
+                    <div className="img-holder">
+                    <img src={Learn} width="80px" height="80px" alt="Learn" />
+                    </div>
+                    <a href="#">
+                        <h5>Why learning matters</h5>
+                        </a>
+                        <div className="d-lg-block d-lg-block">
+                        <p>College is worth it. Your future is worth it. You are worth it.</p>
+                        </div>
+                        {/* <a href="#">Learn why college matters ></a> */}
+                  </div>
+              </div>
+              <div className="col-sm-4">
+                  <div className="col">
+                    <div className="img-holder">
+                    <img src={Credit} width="80px" height="80px" alt="Credit" />
+                    </div>
+                    <a href="#">
+                        <h5>Paying for college</h5>
+                        </a>
+                        <div className="d-lg-block d-lg-block">
+                        <p>There are lots of options available to you. We can help you find them.</p>
+                        </div>
+                        {/* <a href="#">Learn how to pay for college ></a> */}
+                  </div>
+              </div>
+              <div className="col-sm-4">
+                  <div className="col cut-corner">
+                    <div className="img-holder">
+                    <img src={Map} width="80px" height="80px" alt="Map" />
+                    </div>
+                    <a href="#">
+                        <h5>Your college roadmap</h5>
+                        </a>
+                        <div className="d-lg-block d-lg-block">
+                        <p>The path may seem unclear. We can help you find your&nbsp;way.</p>
+                        </div>
+                        {/* <a href="#">Learn how to get to college ></a> */}
+                  </div>
+              </div>
+          </div>
+      </div>
 
-export default IndexPage
+  </section>
+}
